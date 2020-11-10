a3f7bc6a6f5a3c178891829399b5b8408fede9c0
bndkmn <benedekkaman@gmail.com>
2020-11-10 11:01:51 +0100

    l

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 // global variables
-var mapname = 'mapbox.satellite';
+var mapname = 'ckhbrqz200sfm19l8s1kllc5p';
 var dataLayerName = '';
 var wmsLayer = '';
 var maplayer = '';
@@ -61,11 +61,11 @@ var legend = L.control({position: 'bottomright'}); // add legend
 // change map style
 function changeMapStyle(name) {
     //Add new layer to map
-    maplayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    maplayer = L.tileLayer('https://api.mapbox.com/styles/v1/bndkkmn/{style_id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: name,
-        format: 'images/png',
-        accessToken: 'pk.eyJ1IjoiYm9vc3R2b2x0IiwiYSI6ImNrZmptMHpkYTBleXgzM21wemZhd3U5ZjcifQ.-VbXp7FqTI4UaFNpj-Sf8w'
+        style_id: name,
+        format: 'images/jpeg',
+        accessToken: 'pk.eyJ1IjoiYm5ka2ttbiIsImEiOiJja2hicm8yc2IwN2N0MnFuZ3UzcjBoNzFsIn0.DEonH9r2NRX7wpdT4yHNoQ'
     }).addTo(map);
 
     //Move wmslayer to front
@@ -102,17 +102,15 @@ function changeLayer(id) {
     }).addTo(map);
 
     // show legend (if legend is Hauptboden, then do not display because of size)
-    if (dataLayerName !== 'Hauptboden') {
-        legend.onAdd = function () {
-            // create legend
-            var div = L.DomUtil.create('div', 'info legend');
-            var url = 'https://map.geo.tg.ch/proxy/geofy_chsdi3/bodenuebersicht-profile_bohrungen?access_key=YoW2syIQ4xe0ccJA&version=1.3.0&service=WMS&request=GetLegendGraphic&sld_version=1.1.0&layer=' + dataLayerName + '&format=image/png&STYLE=default';
-            div.innerHTML += '<h4>Legende</h4><br>' +
-                '<img src=' + url + ' alt="legend" class="legend">';
-            return div;
-        };
-        legend.addTo(map);
-    }
+    legend.onAdd = function () {
+        // create legend
+        var div = L.DomUtil.create('div', 'info legend');
+        var url = 'https://map.geo.tg.ch/proxy/geofy_chsdi3/bodenuebersicht-profile_bohrungen?access_key=YoW2syIQ4xe0ccJA&version=1.3.0&service=WMS&request=GetLegendGraphic&sld_version=1.1.0&layer=' + dataLayerName + '&format=image/png&STYLE=default';
+        div.innerHTML += '<h4>Legende</h4><br>' +
+            '<img src=' + url + ' alt="legend" class="legend">';
+        return div;
+    };
+    legend.addTo(map);
 }
 
 // generate map
